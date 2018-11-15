4e1291e942126793f7006a97e97c64b661b1481f
kiran.paturi <kiran>
2018-11-15 08:04:47 +0530

    added code

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //ArrayType[] variable name
+        //string[] students = new string[5];
+        //students[0] = "kiran";
+        //students[1] = "Ravi";
+        //students[2] = "John";
+        //students[3] = "Geetha";
+        //students[4] = "Pony";
+
+        //for(int i=0;i<5;i++)
+        //{
+        //    Console.WriteLine(students[i]);
+        //}
+        string[] students = new string[] { "Jan", "Feb", "Mar" };
+        
+        for(int i=0; i < students.Length; i++)
+        {
+            Console.WriteLine(students[i]);
+        }
+        Console.WriteLine("------------FOREACH---------------");
+        //                   collection
+        foreach(string item in students)
+        {
+            Console.WriteLine(item);   
+        }
+        Console.ReadKey();
+    }
+}
+
--- DataTypesAndConditionalStatements/DataTypesAndConditionalStatements/Program.cs
+++ DataTypesAndConditionalStatements/DataTypesAndConditionalStatements/Program.cs
@@ -12,13 +12,14 @@ namespace DataTypesAndConditionalStatements
         static void Main(string[] args)
         {
             //Write a program to print numbers for 0 to 10;
-            for(int i = 1; i <= 10; )
+            for(int i = 1; i <= 100;i++ )
             {
                 Console.WriteLine(i);
-                i = i * 2;
             }
             Console.ReadKey();
         }
+
+
 
     }
 }
